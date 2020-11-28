ee16dc380f28567cf43346867a64990d5cf9e7c5
Glenn Shimoda <gshimoda@ryerson.ca>
2020-11-28 03:09:32 -0500

    Basic PF is working. Will work on showing particles

--- HW3/PF.py
+++ HW3/PF.py
@@ -3,10 +3,11 @@ import random, math
 from numpy import dot, sum, tile, linalg, array, random, eye, zeros, diag, transpose, mean, var, cov
 from numpy.linalg import inv, norm
 import matplotlib.pyplot as plt
+import itertools as it
 
 #Basic Options
 Noise = True
-Linear = False
+Linear = True
 Ellipse = False
 
 #Initialize the pygame
@@ -47,7 +48,8 @@ image = image.convert_alpha()
 #---------------------------------------------------------------ROBOT PARAMETERS-------------------------------------------------------------------
 
 #Initial Conditions
-M = 10  #number of samples
+M = 100  #number of samples
+bins = int(M/10)
 x0 = 6.0
 y0 = 10.0
 theta = math.pi/2.0
@@ -72,13 +74,9 @@ U = array([[r*u_w*math.cos(theta),r*u_w*math.sin(theta)]]).T
 ww = 0.1
 wphi = 0.01
 Q = diag([ww+wphi, ww+wphi])
-# delf_delw = array([T, T])
-# delf_delphi = (T**2)*r*u_w* array([-math.sin(theta), math.cos(theta)])
-# w_k = array([ww*delf_delw[0]+wphi*delf_delphi[0], ww*delf_delw[1]+wphi*delf_delphi[1]])
-# Q_p = diag([w_k[0], w_k[1]])
 
 #Linear measurement parameters
-C = diag([1, 2])
+C = diag([1, 1])
 rx, ry = 0.05, 0.075
 R = diag([rx, ry])
 phi = theta + math.pi/2.0
@@ -87,59 +85,146 @@ dist_norm = 4.0
 #True line
 true_line = [(x0*sc_f, y0*sc_f), (x0, y0)]
 
-def sample(theta):
+def sample(X_prev, U):
     samples = []
-    theta_prev = theta
-    for i in range(len(M)):
-        theta_noise = random.normal(0, wphi)
-        theta_dot = (r/rL)*u_phi + theta_noise
-        theta = (T*theta_dot) + theta_prev
-        U = array([[r*u_w*math.cos(theta),r*u_w*math.sin(theta)]]).T
-
+    for i in range(M):
+        X = X_prev
         noise_x = random.normal(0, ww)
         noise_y = random.normal(0, ww)
         noise = array([[noise_x, noise_y]]).T
-        X_dot = U + noise
-        X = dot(F, X) + (T*X_dot)
-
-        samples.append(X)
+        X_dot = (U + noise).reshape(2,)
+        X = dot(F, X).reshape(2,) + (T*X_dot)#Xk equation 2 from HW2
+        samples.append(X.reshape(2,))  
     return samples
 
-def resample(pred):
+def create_bins(x_dist, y_dist):
+    X_max = max(x_dist)
+    X_min = min(x_dist)
+    Y_max = max(y_dist)
+    Y_min = min(y_dist)
+    max_min = (X_max, X_min, Y_max, Y_min)
+    delta_x = (X_max - X_min)/10
+    delta_y = (Y_max - Y_min)/10
+    delta = [delta_x, delta_y]
+    return max_min, delta
+
+def create_dist(x_dist, y_dist, delta, *max_min):
+    X_max, X_min, Y_max, Y_min = max_min
+    delta_x, delta_y = delta[0], delta[1]
+    px = [0]*bins
+    py = [0]*bins
+    p = []
+    k_list = []
+    for i in range(M):
+        kx = int(math.ceil((x_dist[i]-X_min)/delta_x))
+        ky = int(math.ceil((y_dist[i]-Y_min)/delta_y))
+        if x_dist[i] == X_min:
+            px[0] += 1
+            kx = 1
+        else:
+            px[kx-1] += 1
+        if y_dist[i] == Y_min:
+            py[0] += 1
+            ky = 1
+        else:
+            py[ky-1] += 1
+        k_list.append((kx-1, ky-1))
+        p = list(zip(px, py))
+    return p, k_list
+
+def probability(x_p, y_p, p_dist, delta, *max_min):
+    X_max, X_min, Y_max, Y_min = max_min
+    delta_x, delta_y = delta[0], delta[1]
+    p = []
+    for i in range(M):
+        kx = (x_p[i]-X_min)/delta_x
+        ky = (y_p[i]-Y_min)/delta_y
+        if (kx < 0) or (kx > 10):
+            px = 0
+        else:
+            kx = int(math.ceil(kx))
+            if kx == 0:
+                px = p_dist[0][0]
+            else:
+                px = p_dist[kx-1][0]
+        if (ky < 0) or (ky > 10):
+            py = 0
+        else:
+            ky = int(math.ceil(ky))
+            if ky == 0:
+                py = p_dist[0][1]
+            else:
+                py = p_dist[ky-1][1]
+        prob = (px, py)
+        p.append(prob)
+    return p
+
+def correct(pred):
     correct_samples = []
-    for i in range(len(M)):
-        if Linear == True:
+    for i in range(M):
+        #Predict the no-noise model
+        if Linear is True:
             n_x = random.normal(0, rx)
             n_y = random.normal(0, ry)
             n = array([n_x, n_y]).T
-            Z = dot(C, pred[i]) + n
+            Z = dot(C, pred[i]).reshape(2,) + n
+            correct_samples.append(Z)
+        else:
+            dist = pred[i] - L.reshape(2,)
+            n_w = random.normal(0, ww)
+            n_phi = random.normal(0, wphi)
+            phi = theta + math.pi/2.0
+            phi_n = phi + n_phi
+            
+            dist_norm = norm(dist, ord = 2) + n_w
+            x = 10 + (dist_norm*math.cos(phi_n))
+            y = 10 + (dist_norm*math.sin(phi_n))
+            Z = array([x, y])
             correct_samples.append(Z)
     return correct_samples
+
+def get_weights(prob_p, k_p, prob_c, k_c):
+    w = []
+    for i in range(M):
+        wx = (prob_c[i][0])/(prob_p[k_p[i][0]][0])
+        wy = (prob_c[i][1])/(prob_p[k_p[i][1]][1])
+        w.append((wx, wy))
+    return w
+
+def resample(w, x_prior, y_prior):
+    wx = [x[0] for x in w]
+    wy = [y[1] for y in w]
+    beta, X_post = [], []
+    sumx = sum(wx)
+    sumy = sum(wy)
+    for i in range(M):
+        b_x = sum(wx[0:i+1])/sumx
+        b_y = sum(wy[0:i+1])/sumy
+        beta.append((b_x, b_y))
+    rhox = random.uniform(0, 1)
+    rhoy = random.uniform(0, 1)
+    index_x = [0]*M
+    index_y = [0]*M
+    for i in range(M):
+        indx = len(list(it.takewhile(lambda x: x < rhox, [x[0] for x in beta])))
+        x_new = x_prior[indx]
+        indy = len(list(it.takewhile(lambda x: x < rhoy, [y[1] for y in beta])))
+        y_new = y_prior[indy]
+        X_post.append((x_new, y_new))
+        rhox += 1/M
+        rhoy += 1/M
+        if rhox >= 1:
+            rhox = random.uniform(0, 1)
+        if rhoy >= 1:
+            rhoy = random.uniform(0, 1)
+        index_x[indx] += 1
+        index_y[indy] += 1
+    index = list(zip(index_x, index_y))
+    return beta, X_post, index
 
 def update_text(X, Y, coord):
     X_text = font.render("X:" + str(coord), True, (255, 255, 255))
     screen.blit(X_text, (X, Y))
-
-def kf_predict(X, P, F, Q_p, U):
-    noise_x = random.normal(0, ww)
-    noise_y = random.normal(0, ww)
-    noise = array([[noise_x, noise_y]]).T
-    X_dot = U + noise
-    X = dot(F, X) + (T*X_dot)
-    P = dot(F, dot(P, F.T)) + Q_p
-    return X, P
-
-#Basic correction
-def kf_correct(X, P, Y, C, R):
-    IM = dot(C, X)
-    IS = R + dot(C, dot(P, C.T))
-    K = dot(P, dot(C.T, inv(IS)))
-    X = X + dot(K, (Y-IM))
-    P = P - dot(K, dot(C, P))
-    return (X, P, K, IM, IS)
-
-def range_bearing(d_norm, phi):
-    return array([10+(d_norm*math.cos(phi)), 10+(d_norm*math.sin(phi))])
 
 if Noise == False:
     ww, wphi, rx, ry = 0, 0, 0, 0
@@ -148,7 +233,7 @@ running = True
 screen.fill((0, 0, 0))
 screen.blit(Landmark, (int(scale/2), int(scale/2)))
 R_text = font.render("Radius of circle: 4m", True, (255, 255, 255))
-Q_text = font.render("HW2 Part " + str(part), True, (255, 255, 255))
+Q_text = font.render("HW3 Part " + str(part), True, (255, 255, 255))
 C_text = font.render("Covariance Ellipse Scale: " + '{:01.2f}'.format(cov_scale), True, (255, 255, 255))
 B_text = font.render("Bearing: 90", True, (255, 255, 255))
 screen.blit(R_text, (textX, textY))
@@ -156,82 +241,87 @@ screen.blit(Q_text, (textX, textY+20))
 screen.blit(B_text, (textX, textY+60))
 
 while running:
-    i += 1 
+
+    theta_noise = random.normal(0, wphi)
+    theta_dot = (r/rL)*u_phi + theta_noise
+    theta = (T*theta_dot) + theta
+    U = array([[r*u_w*math.cos(theta),r*u_w*math.sin(theta)]]).T
+
+    predict_samples = sample(X, U)
+
+    x_pred = [x[0] for x in predict_samples]
+    y_pred = [y[1] for y in predict_samples]
+    max_min, delta = create_bins(x_pred, y_pred)
+    prob_p, k_p = create_dist(x_pred, y_pred, delta, *max_min)
+
+    correct_samples = correct(predict_samples)
+    x_cor = [x[0] for x in correct_samples]
+    y_cor = [y[1] for y in correct_samples]
+    max_min_c, delta_c = create_bins(x_cor, y_cor)
+    prob_c, k_c = create_dist(x_cor, y_cor, delta_c, *max_min_c)
+
+    prob_post = probability(x_pred, y_pred, prob_c, delta_c, *max_min_c)
+    weights = get_weights(prob_p, k_p, prob_post, k_c)
+    beta, posterior, indices = resample(weights, x_pred, y_pred)
+    x_post = [x[0] for x in posterior]
+    y_post = [y[1] for y in posterior]
+
+    indx = [x[0] for x in indices]
+    indy = [y[1] for y in indices]
+
+    max_x = max(indx)
+    i_xmax = [i for i, j in enumerate(indx) if j == max_x]
+
+    max_y = max(indy)
+    i_ymax = [i for i, j in enumerate(indy) if j == max_y]
+
+    avgx = 0
+    for i in i_xmax:
+        avgx += x_pred[i]
+    avgx = avgx/len(i_xmax)
+
+    avgy = 0
+    for i in i_ymax:
+        avgy += y_pred[i]
+    avgy = avgy/len(i_ymax)
+
+    X = array([[avgx, avgy]]).T
+
+    print(avgx, avgy, theta*180/(math.pi))
+
+    if theta < 0:
+        theta = math.pi*2.0
+
+    # i += 1 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     pygame.time.wait(125)
+    
+    true_line[1] = (int(avgx*sc_f), int(avgy*sc_f))
+    pygame.draw.lines(screen, (0, 0, 255), False, true_line, 3)
+    true_line[0] = true_line[1]
 
-    predict_samples = sample(theta)    
-
-    X, P = kf_predict(X, P, F, Q_p, U)
-
-    if i%8 == 0:
-        if Linear == True:
-            n_x = random.normal(0, rx)
-            n_y = random.normal(0, ry)
-            n = diag([n_x, n_y])
-            Z = dot(C, X) + n
-
-            X, P, K, IM, IS = kf_correct(X, P, Z, C, R)
-            true_line[1] = (int(X[0][0]*sc_f), int(X[1][0]*sc_f))
-            pygame.draw.lines(screen, (0, 0, 255), False, true_line, 3)
-            true_line[0] = true_line[1]
-        else:
-            dist = X - L
-            dist = dist[:, 0]
-            n_w = random.normal(0, ww)
-            n_phi = random.normal(0, wphi)
-            dist_norm = norm(dist, ord = 2)
-            dist_norm_n = dist_norm + n_w
-            phi = theta + math.pi/2.0
-            phi_n = phi + n_phi
-
-            #Update covariance matrix R
-            delg_delw = array([math.cos(phi), math.sin(phi)])
-            delg_delphi = dist_norm* array([-math.sin(phi), math.cos(phi)])
-
-            n_k = array([ww*delg_delw[0]+wphi*delg_delphi[0], ww*delg_delw[1]+wphi*delg_delphi[1]])
-            R_p = diag([n_k[0], n_k[1]])
-
-            Z = range_bearing(dist_norm_n, phi_n)
-            IM = range_bearing(dist_norm, phi)
-            G1 = array([(X[0, 0] - 10.0)*math.cos(phi), (X[1,0] - 10.0)*math.cos(phi)])
-            G2 = array([(X[0, 0] - 10.0)*math.sin(phi), (X[1,0] - 10.0)*math.sin(phi)])
-            G = (1.0/dist_norm)*array([G1, G2])
-            #G = G.reshape(2, 2)
-
-            X, P, K, IM, IS = kf_correct_bm(X, P, Z, G, R_p, IM)
-            true_line[1] = (int(X[0, 0]*sc_f), int(X[1, 0]*sc_f))
-            pygame.draw.lines(screen, (0, 0, 255), False, true_line, 3)
-            true_line[0] = true_line[1]
-
-    Xx = (X[0][0]*sc_f)
-    Xy = (X[1][0]*sc_f)
-
-    Px = (P[0][0])
-    Py = (P[1][1])
+    # Px = (P[0][0])
+    # Py = (P[1][1])
 
     dist = X - L
     dist = dist[:,0]
     dist_norm = norm(dist, ord = 2)
 
-    #print('Angle %2.2f Norm: %2.2f X: %2.2f Y: %2.2f' %(theta*180.0/math.pi, dist_norm, X[0][0], X[1][0]))
-    
-    if theta < - 0:
-        theta = math.pi*2.0
+    # #print('Angle %2.2f Norm: %2.2f X: %2.2f Y: %2.2f' %(theta*180.0/math.pi, dist_norm, X[0][0], X[1][0]))
 
-    screen.blit(robot, (int(Xx), int(Xy)))
+    screen.blit(robot, (int(avgx*sc_f), int(avgy*sc_f)))
 
-    #print('CovX %2.4f CovY %2.4f' %(Px, Py))
-    if Ellipse == True:
-        try:
-            pygame.draw.ellipse(screen, (0, 255, 0), (int(Xx-(Px*cov_size/2)), int(Xy-(Py*cov_size/2)), int(Px*cov_size), int(Py*cov_size)), 1)
-            # ellipse_r = pygame.transform.rotate(image, 45)
-            # screen.blit(ellipse_r, (int(rx), int(ry)))
-        except:
-            print('CovX %2.4f CovY %2.4f' %(Px, Py))
+    # #print('CovX %2.4f CovY %2.4f' %(Px, Py))
+    # if Ellipse == True:
+    #     try:
+    #         pygame.draw.ellipse(screen, (0, 255, 0), (int(Xx-(Px*cov_size/2)), int(Xy-(Py*cov_size/2)), int(Px*cov_size), int(Py*cov_size)), 1)
+    #         # ellipse_r = pygame.transform.rotate(image, 45)
+    #         # screen.blit(ellipse_r, (int(rx), int(ry)))
+    #     except:
+    #         print('CovX %2.4f CovY %2.4f' %(Px, Py))
 
     n_phi = random.normal(0, wphi)
     Bearing = (theta + math.pi/2.0 + n_phi)*180.0/math.pi
